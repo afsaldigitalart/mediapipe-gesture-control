107ae20114e828fc7ce1d032cace62f1e19c85fc
Afsal <afsalshajibismi@gmail.com>
2025-02-16 12:41:45 +0530

    added main.py and empty arduino.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import cv2
+import mediapipe as mp
+
+
+def palm_open(landmarks):
+    tip_ids = [4, 8, 12, 16, 20]  
+    pip_ids = [3, 6, 10, 14, 18]  
+    open_fingers = 0
+
+    if landmarks.landmark[tip_ids[0]].x < landmarks.landmark[pip_ids[0]].x:
+        open_fingers += 1
+
+
+    for i in range(1, 5):
+        if landmarks.landmark[tip_ids[i]].y < landmarks.landmark[pip_ids[i]].y:
+            open_fingers += 1
+
+    return open_fingers >= 4 
+
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+hands = mp_hands.Hands(max_num_hands=1,min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+            if handedness.classification[0].label == "Right":
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                mp_draw.DrawingSpec(color=(0, 0, 255), thickness=3, circle_radius=2),  
+                mp_draw.DrawingSpec(color=(255, 255, 255), thickness=2))
+
+
+                wrist = hand_landmarks.landmark[0]
+                index = hand_landmarks.landmark[8]
+                index_base = hand_landmarks.landmark[5]
+                middle_base = hand_landmarks.landmark[9]
+                
+
+                pointing_threshold = 0.1
+                
+                if index.x < wrist.x - pointing_threshold and index.x < index_base.x  - pointing_threshold  and index.x < middle_base.x - pointing_threshold :  # Tip is above the base
+                        cv2.putText(frame, "Pointing Left", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 
+                                    1, (0, 255, 0), 2, cv2.LINE_AA)
+                elif index.x > wrist.x  and index.x > index_base.x - pointing_threshold  and index.x - pointing_threshold  > middle_base.x - pointing_threshold  :  # Tip is below the base
+                        cv2.putText(frame, "Pointing Right", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 
+                                    1, (0, 0, 255), 2, cv2.LINE_AA)
+                    
+                else:
+
+                    if palm_open(hand_landmarks):
+                        cv2.putText(frame, "Palm Open", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 
+                                    1, (0, 255, 0), 2, cv2.LINE_AA)
+
+                    else:
+                        if not palm_open(hand_landmarks):cv2.putText(frame, "Palm Closed", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 
+                                    1, (0, 255, 0), 2, cv2.LINE_AA, )
+                    
+
+
+    cv2.imshow("Hand Tracking AI", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
